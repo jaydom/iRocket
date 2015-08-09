b07019427730ef10002dfdb1d1c6c20701985283
power_wind@126.com <power_wind@126.com>
2015-08-10 00:30:27 +0800

    buddle message css style

--- public/javascripts/angular/directives.js
+++ public/javascripts/angular/directives.js
@@ -20,3 +20,28 @@ phonecatDirectives.directive('checkUser', ['$rootScope', '$location', '$user', f
         }
     }
 }]);
+
+phonecatDirectives.directive('ckeditor', function() {
+    return {
+        require : '?ngModel',
+        link : function(scope, element, attrs, ngModel) {
+            var ckeditor = CKEDITOR.replace(element[0], {
+
+            });
+            if (!ngModel) {
+                return;
+            }
+            ckeditor.on('instanceReady', function() {
+                ckeditor.setData(ngModel.$viewValue);
+            });
+            ckeditor.on('pasteState', function() {
+                scope.$apply(function() {
+                    ngModel.$setViewValue(ckeditor.getData());
+                });
+            });
+            ngModel.$render = function(value) {
+                ckeditor.setData(ngModel.$viewValue);
+            };
+        }
+    };
+});
